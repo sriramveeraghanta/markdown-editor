36e3caa4141be3d3393b96aa55c60a4503424601
sriram4dante <veeraghanta.sriram@gmail.com>
2017-02-12 03:38:20 +0530

    markdown editor working

--- static/react/markdown.js
+++ static/react/markdown.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import showdown from 'showdown'
 
 
 class Markdown extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = {
 
+		this.state = {
+			markdown : "",
+			html_content: ""
 		}
 	}
 	componentDidMount() {
 		
 	}
-	loadTracksDataFromServer(){
-		
+	handleChange(event){
+		this.setState({markdown:event.target.value});
+		var converter = new showdown.Converter();
+		this.setState({html_content: converter.makeHtml(this.state.markdown)});
 	}
 	render(){
 		return(
@@ -20,9 +25,10 @@ class Markdown extends React.Component{
 				<div className="row">
 					<h1 className="page-header text-center">MarkDown Editor</h1>
 					<div className="col-sm-6">
-						<textarea className="form-control"></textarea>
+						<textarea className="form-control" value={this.state.markdown} onChange={this.handleChange.bind(this)}></textarea>
 					</div>
 					<div className="col-sm-6">
+						<div dangerouslySetInnerHTML={{__html: this.state.html_content}} />
 					</div>
 				</div>
 			</div>
