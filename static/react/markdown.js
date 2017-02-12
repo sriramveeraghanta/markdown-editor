32ee629c247834fcbb9c49053a472a4bddc07562
sriram4dante <veeraghanta.sriram@gmail.com>
2017-02-12 22:06:55 +0530

    Last char bugfix with a callback function

--- static/react/markdown.js
+++ static/react/markdown.js
@@ -7,28 +7,30 @@ class Markdown extends React.Component{
 		super(props);
 
 		this.state = {
-			markdown : "",
-			html_content: ""
+			markdown: "",
 		}
 	}
 	componentDidMount() {
 		
 	}
 	handleChange(event){
-		this.setState({markdown:event.target.value});
-		var converter = new showdown.Converter();
-		this.setState({html_content: converter.makeHtml(this.state.markdown)});
+		this.setState({markdown:event.target.value}, () =>{
+			var converter = new showdown.Converter();
+			this.setState({html_content: converter.makeHtml(this.state.markdown)});
+		});	
 	}
 	render(){
 		return(
-			<div className="container">
-				<div className="row">
-					<h1 className="page-header text-center">MarkDown Editor</h1>
-					<div className="col-sm-6">
-						<textarea className="form-control" value={this.state.markdown} onChange={this.handleChange.bind(this)}></textarea>
-					</div>
-					<div className="col-sm-6">
-						<div dangerouslySetInnerHTML={{__html: this.state.html_content}} />
+			<div className="page-content">
+				<div className="container">
+					<div className="row">
+						<h1 className="page-header text-center">MarkDown Editor</h1>
+						<div className="col-sm-6 editor-col">
+							<textarea className="markdown-editor" value={this.state.markdown} placeholder="Start typing from here." onChange={this.handleChange.bind(this)}></textarea>
+						</div>
+						<div className="col-sm-6 html-col">
+							<div dangerouslySetInnerHTML={{__html: this.state.html_content}} />
+						</div>
 					</div>
 				</div>
 			</div>
